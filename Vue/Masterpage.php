7d147f307ee63d5f67597b6fa3da834955b6d353
YANKYkez <yarnoghislain@yahoo.fr>
2017-11-30 15:22:13 +0100

    realisation du header

---
+++ Vue/Masterpage.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" href="css/style.css"/>
+<link rel="stylesheet" href="css/materialize.css"/>
+<link rel="stylesheet" href="css/materialize.min.css"/>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LAETICIA GUEFACK G
+ * Date: 28/11/2017
+ * Time: 17:02
+ */
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/materialize.js"></script>
+<script src="js/materialize.min.js"></script>
---
+++ Vue/header.php
@@ -0,0 +1,88 @@
+<?php include 'Masterpage.php'?>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<div class="row">
+    <div class="col s12 m12 l12" style="background-color: #000000; color: #ffffff; text-align: center">
+        <a href="#!" class="brand-logo center">
+            <img  src="Data/Images/Epee.Line_Logo(72ppp).png" style="width: 12%; margin-bottom: 1%;margin-top: 1%" alt="immobilier et financement cameroun"/>
+        </a>
+    </div>
+</div>
+
+<div class="row" style="background-color:#212121; color: #ffffff; text-align: center; margin-top: -1.3%">
+    <div class="container">
+        <div class="col s12 m2 l2">
+            <ul class="left hide-on-med-and-down">
+                <li>
+
+                    <span style="text-align: center; margin-top: 1%">
+                        <a href="" style="color: #ffffff"><i class="small material-icons">home</i><br/>ACCUEIL
+                        </a>
+
+                    </span>
+                </li>
+            </ul>
+        </div>
+        <div class="col s12 m2 l2">
+            <ul class="left hide-on-med-and-down">
+                <li><a href="" style="color: #ffffff"><i class="small material-icons">youtube_searched_for</i><br/>ACHETER
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col s12 m2 l2">
+            <ul class="left hide-on-med-and-down">
+                <li><a href="" style="color: #ffffff"><i class="small material-icons">vpn_key</i><br/>VENDRE
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col s12 m2 l2">
+            <ul class="left hide-on-med-and-down">
+                <li><a href="" style="color: #ffffff"><i class="small material-icons">trending_up</i><br/>SIMULATEUR
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col s12 m2 l2">
+            <ul class="left hide-on-med-and-down">
+                <li><a href="" style="color: #ffffff"><i class="small material-icons">domaint</i><br/>LOUER
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="col s12 m2 l2">
+            <ul class="left hide-on-med-and-down">
+                <li><a href="" style="color: #ffffff"><i class="small material-icons">receipt</i><br/>CONTACT
+
+
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+
+<style>
+
+    ul:hover{
+        background-color: #cca653;
+        width: 65%;
+        height: 8%;
+        border-radius: 2px;
+    }
+</style>
+
+
+
+
+
+
+
+
+
